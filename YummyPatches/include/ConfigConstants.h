c30d45873da57a96d7d47b167ea7c7a59ccf0bcc
Foma Mironenko <fomius2000@yandex.ru>
2020-04-11 19:44:57 +0300

    flags for output

---
+++ YummyPatches/include/ConfigConstants.h
@@ -0,0 +1,14 @@
+//
+// Created by fomius2000 on 06.04.20.
+//
+
+#ifndef KLEE_CONFIGCONSTANTS_H
+#define KLEE_CONFIGCONSTANTS_H
+
+namespace yummy
+{
+    const bool PRINT_CONSTRAINTS =  false;    // ConstrainManager::~ConstraintManager
+    const bool PRINT_VALUES =       true;     // MemoryObject::~MemoryObject
+}
+
+#endif //KLEE_CONFIGCONSTANTS_H
